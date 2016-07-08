6faea31ca2166c39c04589b522b72827bcc940d7
Andrea <andrea66x@hotmail.com>
2016-07-08 01:24:53 -0500

    Campos de datos personales en la clase Formulario

--- app/src/main/java/fiec/ndr/Formulario.java
+++ app/src/main/java/fiec/ndr/Formulario.java
@@ -4,34 +4,52 @@ package fiec.ndr;
  * Created by Andrea on 07/07/2016.
  */
 public class Formulario {
-    private String nombres;
-    private String apellidos;
-    private String codigo;
+    private String tipo,codigo,nombres,apellidos,sexo,fec_nac,est_civil,origen;
+    private int edad,telefono;
+
 
     public Formulario() {
     }
 
-    public String getCodigo() {
-        return codigo;
-    }
+    /********  Datos Personales  *********/
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
+    public String getTipo(){return tipo;}
 
-    public String getNombres() {
-        return nombres;
-    }
+    public void setTipo(String tipo){this.tipo = tipo;}
 
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
+    public String getCodigo() {return codigo;}
 
-    public String getApellidos() {
-        return apellidos;
-    }
+    public void setCodigo(String codigo) {this.codigo = codigo;}
 
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
+    public String getNombres() {return nombres;}
+
+    public void setNombres(String nombres) {this.nombres = nombres;}
+
+    public String getApellidos() {return apellidos;}
+
+    public void setApellidos(String apellidos) {this.apellidos = apellidos;}
+
+    public String getSexo(){return sexo;}
+
+    public void setSexo(String sexo){this.sexo = sexo;}
+
+    public String getFec_nac() {return fec_nac;}
+
+    public void setFec_nac(String fec_nac) {this.fec_nac = fec_nac;}
+
+    public int getEdad(){return edad;}
+
+    public void setEdad(int edad){this.edad = edad;}
+
+    public int getTelefono(){return telefono;}
+
+    public void setTelefono(int telefono){this.telefono = telefono;}
+
+    public String getEst_civil() {return est_civil;}
+
+    public void setEst_civil(String est_civil) {this.est_civil = est_civil;}
+
+    public String getOrigen() {return origen;}
+
+    public void setOrigen(String origen) {this.origen = origen;}
 }
