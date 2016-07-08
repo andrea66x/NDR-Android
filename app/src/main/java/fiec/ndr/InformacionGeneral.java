1b32d0ccb36b3808250f47e2f9251c63fdc4fe2a
rolansor <rolansor@espol.edu.ec>
2016-07-07 23:05:51 -0500

    Fragment de Vivienda.

--- app/src/main/java/fiec/ndr/InformacionGeneral.java
+++ app/src/main/java/fiec/ndr/InformacionGeneral.java
@@ -216,6 +216,66 @@ public class InformacionGeneral extends AppCompatActivity{
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_vivienda, container, false);
+
+            //Spinner para la provincia:
+            Spinner spinner_provincia = (Spinner) rootView.findViewById(R.id.datos_provincia);
+            List<String> provincias = new ArrayList<String>();
+            provincias.add("Guayas");
+            provincias.add("Esmeraldas");
+            provincias.add("Los Ríos");
+            provincias.add("El Oro");
+            provincias.add("Manabí");
+            ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>(rootView.getContext(), R.layout.spinners, provincias);
+            dataAdapter1.setDropDownViewResource(R.layout.spinners);
+            spinner_provincia.setAdapter(dataAdapter1);
+
+            //Spinner para el canton:
+            Spinner spinner_canton = (Spinner) rootView.findViewById(R.id.datos_canton);
+            List<String> cantones = new ArrayList<String>();
+            cantones.add("Balzar");
+            cantones.add("Guayaquil");
+            cantones.add("Manta");
+            cantones.add("Portoviejo");
+            cantones.add("Esmeraldas");
+            cantones.add("Balzar");
+            cantones.add("Guayaquil");
+            cantones.add("Manta");
+            cantones.add("Portoviejo");
+            cantones.add("Esmeraldas");
+            cantones.add("Balzar");
+            cantones.add("Guayaquil");
+            cantones.add("Manta");
+            cantones.add("Portoviejo");
+            cantones.add("Esmeraldas");
+            cantones.add("Balzar");
+            cantones.add("Guayaquil");
+            cantones.add("Manta");
+            cantones.add("Portoviejo");
+            cantones.add("Esmeraldas");
+            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(rootView.getContext(), R.layout.spinners, cantones);
+            dataAdapter2.setDropDownViewResource(R.layout.spinners);
+            spinner_canton.setAdapter(dataAdapter2);
+
+            //Spinner para la vivienda:
+            Spinner spinner_vivienda = (Spinner) rootView.findViewById(R.id.datos_vivienda);
+            List<String> vivienda = new ArrayList<String>();
+            vivienda.add("Ciudad");
+            vivienda.add("Campo");
+            vivienda.add("Alrededor de la Ciudad");
+            ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(rootView.getContext(), R.layout.spinners, vivienda);
+            dataAdapter3.setDropDownViewResource(R.layout.spinners);
+            spinner_vivienda.setAdapter(dataAdapter3);
+
+            //Spinner para el agua:
+            Spinner spinner_agua = (Spinner) rootView.findViewById(R.id.datos_agua);
+            List<String> agua = new ArrayList<String>();
+            agua.add("Intubada");
+            agua.add("Cisterna");
+            agua.add("Tanquero");
+            ArrayAdapter<String> dataAdapter4 = new ArrayAdapter<String>(rootView.getContext(), R.layout.spinners, agua);
+            dataAdapter4.setDropDownViewResource(R.layout.spinners);
+            spinner_agua.setAdapter(dataAdapter4);
+
             return rootView;
         }
     }
