93b21dee6122e29b5672d6191c6c59c35a241c06
Andrea <andrea66x@hotmail.com>
2016-07-27 17:53:01 -0500

    clases para demás formularios

---
+++ app/src/main/java/fiec/ndr/Formularios/Frm_Laboratorio.java
@@ -0,0 +1,9 @@
+package fiec.ndr.Formularios;
+
+/**
+ * Created by Andrea on 27/07/2016.
+ */
+public class Frm_Laboratorio {
+    public Frm_Laboratorio() {
+    }
+}
---
+++ app/src/main/java/fiec/ndr/Formularios/Frm_Medidas.java
@@ -0,0 +1,66 @@
+package fiec.ndr.Formularios;
+
+/**
+ * Created by Andrea on 27/07/2016.
+ */
+public class Frm_Medidas {
+    private float peso, estatura, cintura, caderas, imc, indice_cint_cad, peso_sal;
+    public Frm_Medidas() {
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public float getCintura() {
+        return cintura;
+    }
+
+    public void setCintura(float cintura) {
+        this.cintura = cintura;
+    }
+
+    public float getCaderas() {
+        return caderas;
+    }
+
+    public void setCaderas(float caderas) {
+        this.caderas = caderas;
+    }
+
+    public float getImc() {
+        return imc;
+    }
+
+    public void setImc(float imc) {
+        this.imc = imc;
+    }
+
+    public float getIndice_cint_cad() {
+        return indice_cint_cad;
+    }
+
+    public void setIndice_cint_cad(float indice_cint_cad) {
+        this.indice_cint_cad = indice_cint_cad;
+    }
+
+    public float getPeso_sal() {
+        return peso_sal;
+    }
+
+    public void setPeso_sal(float peso_sal) {
+        this.peso_sal = peso_sal;
+    }
+}
